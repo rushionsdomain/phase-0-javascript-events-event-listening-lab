26d669c7ed4b5677696f3d1a1d006fbcaf888614
Rushion <rushionchegge@gmail.com>
2024-07-10 14:45:31 +0300

    created js file that meets the tests

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+  const button = document.getElementById("button");
+
+  function handleClick() {
+    console.log("Button was clicked!");
+  }
+
+  button.addEventListener("click", handleClick);
 }
+
+addingEventListener();
